b94b423a0a85d423ce342d463b79ff67cc178ade
thenampetch <77654013+Thenampetch@users.noreply.github.com>
2023-03-17 15:28:25 +0700

    Add files via upload

---
+++ HW06/src/BST.java
@@ -0,0 +1,247 @@
+
+public class BST {
+	BSTNode root;
+	int size;
+
+	public BST() {
+		root = null;
+		size = 0;
+	}
+
+	public BST(BSTNode root, int size) {
+		this.root = root;
+		this.size = size;
+	}
+
+	public boolean isEmpty() {
+		return size == 0;
+	}
+
+	public void makeEmpty() {
+		root = null;
+		size = 0;
+	}
+
+	public Iterator find(int v) {
+		BSTNode temp = root;
+
+		while (temp != null && temp.data != v) {
+			if (v < temp.data) {
+				temp = temp.left;
+			} else {
+				temp = temp.right;
+			}
+		}
+		if (temp == null) // not found
+			return null;
+		return new TreeIterator(temp);
+	}
+
+	public Iterator insert(int v) {
+		BSTNode parent = null;
+		BSTNode temp = root;
+
+		// This first part is almost the same as find,
+		// but it has an extra pointer called parent.
+		while (temp != null && temp.data != v) {
+			if (v < temp.data) {
+				parent = temp;
+				temp = temp.left;
+
+			} else {
+				parent = temp;
+				temp = temp.right;
+
+			}
+		}
+
+		if (temp == null) {
+			BSTNode n = new BSTNode(v, null, null, parent);
+			if (parent == null) {
+				root = n;
+			} else if (v < parent.data) {
+				parent.left = n;
+			} else {
+				parent.right = n;
+			}
+			size++;
+			return new TreeIterator(n);
+		} else {
+			// we do nothing since
+			// we don't want to add duplicated data.
+			return null;
+		}
+
+	}
+
+	public void remove(int v) {
+		BSTNode parent = null;
+		BSTNode temp = root;
+
+		TreeIterator i = (TreeIterator) find(v);
+		if (i == null) { // not found, we can not remove it
+			return;
+		}
+
+		temp = i.currentNode;
+		parent = temp.parent;
+
+		// otherwise, we remove the value.
+		size--;
+		if (temp.left == null && temp.right == null) {// both subtrees are empty
+			if (parent == null) {
+				root = null;
+			} else if (parent.left == temp) {
+				parent.left = null;
+			} else {
+				parent.right = null;
+			}
+		} else if (temp.left == null && temp.right != null) {// only right child
+			if (parent == null) {
+				root = temp.right;
+				root.parent = null;
+			} else if (parent.right == temp) {
+				BSTNode n = temp.right;
+				n.parent = parent;
+				parent.right = n;
+				// temp.right = null;
+				// temp.parent = null;
+			} else {// parent.left == temp
+				BSTNode n = temp.right;
+				n.parent = parent;
+				parent.left = n;
+			}
+		} else if (temp.right == null && temp.left != null) {
+			if (parent == null) {
+				root = temp.left;
+				root.parent = null;
+			} else if (parent.right == temp) {
+				BSTNode n = temp.left;
+				n.parent = parent;
+				parent.right = n;
+			} else {
+				BSTNode n = temp.left;
+				n.parent = parent;
+				parent.left = n;
+
+			}
+
+		} else {// temp has two subtrees
+			BSTNode n = temp.right;
+			TreeIterator itr = (TreeIterator)(findMin(n));
+			BSTNode minInSubtree = itr.currentNode;
+
+			temp.data = minInSubtree.data;
+
+			BSTNode parentOfMin = minInSubtree.parent;
+			if (parentOfMin.left == minInSubtree) {
+				parentOfMin.left = minInSubtree.right;
+
+			} else { // parentOfMin.right == minInSubtree
+				parentOfMin.right = minInSubtree.right;
+
+			}
+
+			if (minInSubtree.right != null) {
+				minInSubtree.right.parent = parentOfMin;
+			}
+
+		}
+	}
+
+	public Iterator findMin() {
+		BSTNode temp = root;
+		if(temp == null)
+			return null;
+		while (temp.left != null) {
+			temp = temp.left;
+		}
+		Iterator itr = new TreeIterator(temp);
+		return itr;
+	}
+	
+	public Iterator findMin(BSTNode n) {
+		BSTNode temp = n;
+		if(temp == null)
+			return null;
+		while (temp.left != null) {
+			temp = temp.left;
+		}
+		Iterator itr = new TreeIterator(temp);
+		return itr;
+	}
+	
+	public Iterator findMax() {
+		BSTNode temp = root;
+		if(temp == null)
+			return null;
+		while (temp.right != null) {
+			temp = temp.right;
+		}
+		Iterator itr = new TreeIterator(temp);
+		return itr;
+	}
+	
+	public Iterator findMax(BSTNode n) {
+		BSTNode temp = n;
+		if(temp == null)
+			return null;
+		while (temp.right != null) {
+			temp = temp.right;
+		}
+		Iterator itr = new TreeIterator(temp);
+		return itr;
+	}
+
+	public void addLeftSubTreeToMin(BST subtree) {
+		//TODO
+		//FILL CODE
+		if (subtree == null || subtree.root == null) {
+			return;
+		}
+		//if tree is empty subtree is tree.
+		if (root == null) {
+			root = subtree.root;
+			size = subtree.size;
+			return;
+		}
+		//insert subtree to the leftmost.
+		//find max node of subtree.
+		BSTNode maxsubtree = ((TreeIterator) subtree.findMax()).currentNode;
+		//find leftmost of tree.
+		BSTNode leftmost = ((TreeIterator) this.findMin()).currentNode;
+		//check BST validation.
+		if (leftmost.data > maxsubtree.data) {
+			leftmost.left = subtree.root;
+			size += subtree.size;
+		}
+		return;
+	}
+
+
+	public static void main(String[] args) throws Exception {
+		//Printing example. 
+		//You can print how the tree looks to help with debugging.
+		
+		BSTNode r = new BSTNode(9);
+		BST t = new BST(r, 1);
+		
+		BSTNode k = new BSTNode(6);
+		BST t1 = new BST(k,1);
+		//t.insert(9);
+		t.insert(7);
+		t.insert(11);
+		t.insert(8);
+		t.insert(10);
+		t.insert(12);
+		
+		t1.insert(1);
+		t1.insert(9);
+		BTreePrinter.printNode(t.root);
+		BTreePrinter.printNode(t1.root);
+
+		
+
+	}
+
+}
